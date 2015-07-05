9dee5b58b219ec8e83f570cf948c5e6d2632cc09
Tim Shaw <tim.shaw@noblestudios.com>
2015-07-05 03:08:45 -0700

    Added ability for cache to remove oldest added item if the cache is getting full

--- assets/js/models/Cache.js
+++ assets/js/models/Cache.js
@@ -5,15 +5,26 @@ App.models.Cache = Backbone.Model.extend({
     // Populate/create initial cache bucket
     var cache  = localStorage.getItem( 'cache_' + bucket );
 
+    // Likewise with internal added array (keeps track of when items were added
+    // to the cache)
+    var added  = localStorage.getItem( 'cache_added_' + bucket );
+
     try {
       this.cache = JSON.parse( cache );
     } catch ( err ) {
       console.error( 'Error parsing cache bucket: ', err );
     }
 
+    try {
+      this.cacheAdded = JSON.parse( added );
+    } catch ( err ) {
+      console.error( 'Error parsing cache added array: ', err );
+    }
+
     // If it doesn't exist, create a blank object (but don't bother storing it
     // yet until something is actually pushed to it)
     if ( ! this.cache ) { this.cache = {}; }
+    if ( ! this.cacheAdded ) { this.cacheAdded = []; }
   },
 
   /**
@@ -44,22 +55,70 @@ App.models.Cache = Backbone.Model.extend({
    * @param {Mixed} value Value to set with key
    */
   setValue: function ( key, value ) {
-    this.cache[ key ] = value;
+    // First thing, check if there's room in the cache.  If there's not, remove
+    // the oldest item added to make room, then proceed.
+    this.clearCacheSpace();
 
+    // Then move on to adding this item
+    this.cache[ key ] = value;
+    this.cacheAdded.push( key );
+
+    // Then save the current cache
+    this.saveCache();
+  },
+
+  /**
+   * Save the current cache contents (persist the contents to localStorage).
+   */
+  saveCache: function () {
     try {
       var update = JSON.stringify( this.cache );
 
       // Save the current version of the cache
       localStorage.setItem( 'cache_' + this.bucket, update );
+
+      // Update the added array as well (but only if adding the actual item
+      // works)
+      try {
+        var addedUpdate = JSON.stringify( this.cacheAdded );
+
+        localStorage.setItem( 'cache_added_' + this.bucket, addedUpdate );
+      } catch ( err ) {
+        console.error( 'Unable to save cache added array: ', err );
+      }
     } catch ( err ) {
       console.error( 'Unable to save cache bucket: ', err );
     }
   },
 
   /**
-   * Flush the cache completely.
+   * Make sure localStorage is not full and clear junk out if it is.  Will
+   * remove oldest added item if necessary to make room for new incoming item.
+   */
+  clearCacheSpace: function () {
+    // First check the cache size.  This is based on the fact that localStorage
+    // stores approximately 2.5 million UTF-16 characters.
+    var used = ( JSON.stringify( localStorage ).length / 2500000 );
+
+    if ( used >= 1 ) {
+      // Blam the oldest item in the added queue.
+      var key = this.cacheAdded.shift();
+
+      if ( key ) {
+        delete this.cache[ key ];
+
+        // And save the updated cache
+        this.saveCache();
+      }
+    }
+  },
+
+  /**
+   * Flush the cache completely.  Kills both the cache itself as well as the
+   * added array.
    */
   flush: function () {
     localStorage.removeItem( 'cache_' + this.bucket );
+    localStorage.removeItem( 'cache_added_' + this.bucket );
   }
 });
